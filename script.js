08adf8233bd74854b53d8c844a494353bda9d4af
Tanner Roughton <couchgatordev@gmail.com>
2020-06-23 23:02:36 -0400

    dashboard

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-var mini = true;function toggleSidebar() {if (mini) {console.log(“opening sidebar”);document.getElementById(“mySidebar”).style.width = “250px”;document.getElementById(“main”).style.marginLeft = “250px”;this.mini = false;} else {console.log(“closing sidebar”);document.getElementById(“mySidebar”).style.width = “100px”;document.getElementById(“main”).style.marginLeft = “100px”;this.mini = true; }}
+var mini = true;
+
+function toggleSidebar() {
+  if (mini) {
+    console.log("opening sidebar");
+    document.getElementById("mySidebar").style.width = "200px";
+    document.getElementById("main").style.marginLeft = "200px";
+    this.mini = false;
+  } else {
+    console.log("closing sidebar");
+    document.getElementById("mySidebar").style.width = "70px";
+    document.getElementById("main").style.marginLeft = "70px";
+    this.mini = true;
+  }
+}
